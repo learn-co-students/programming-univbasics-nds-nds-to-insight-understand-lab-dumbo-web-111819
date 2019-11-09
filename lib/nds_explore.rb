c161bc1f231cfb80f0dda6f7706d2319cad55540
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-09 23:10:27 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,20 +1,28 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'pp'
 require 'directors_database'
+require 'pp'
+
 # Call the method directors_database to retrieve the NDS
+pp directors_database
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  pp nds
+  pp nds 
 end
 
 def print_first_directors_movie_titles
-  movies = directors_database[0][:movies]
-  index = 0
-
-  while index < movies.length do
-    titles = movies[index][:title]
-    puts titles
-    index +=1
-  end
+  # When exploring the data structure using custom methods, print_first_directors_movie_titles loops over Spielberg’s movies and prints out their names with newlines
+  spiel_films = directors_database[0][:movies]
+  row_index = 0 
+  while row_index < spiel_films.length do 
+    puts spiel_films[row_index][:title]
+    row_index += 1 
+  end 
 end
+
+
+
+
+
+
+
