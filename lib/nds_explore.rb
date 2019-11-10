f00c1ff0c2198bf0c8a56ef29842a00d3a451e85
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-10 19:26:52 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,18 +11,26 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  # When exploring the data structure using custom methods, print_first_directors_movie_titles loops over Spielberg’s movies and prints out their names with newlines
-  spiel_films = directors_database[0][:movies]
-  row_index = 0 
-  while row_index < spiel_films.length do 
-    puts spiel_films[row_index][:title]
-    row_index += 1 
-  end 
+  movie_index = 0 
+  spiel_film = directors_database[0][:movies]
+  while movie_index < spiel_film.length do 
+    puts spiel_film[movie_index][:title]
+    movie_index += 1
+  end
 end
 
-
-
-
-
+=begin
+directors_database
+row_index = 0 
+while row_index < directors_database.length do 
+  column_index = 0 
+  puts "Row #{row_index}: #{directors_database[row_index][:name]}" 
+  while column_index < directors_database[row_index].length do 
+    puts "\t\t Row #{row_index}, Column #{column_index}: #{directors_database[row_index][:movies][column_index]}"
+    column_index += 1 
+  end 
+  row_index += 1 
+end 
+=end
 
 
